421182e8f5e013ad39efa56c72b8b903e283bbec
Rahul T V <61172472+rahulram21@users.noreply.github.com>
2024-04-23 05:17:38 +0000

    Cleared the issue

--- newCpp.cpp
+++ newCpp.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+class student{
+    private:
+        double num;
+        double privateMethod(double num){
+            return num+1;
+        }
+    public:
+        double get(){return num;}
+        void set(double n){num = n;}
+        void publicMethod(){
+            privateMethod(num);
+        }
+};
+
 int main(){
-    cout<<"Hello"<<endl;
-    return 0;
+    student std1;
+    std1.set(7);
+    cout<<std1.get()<<endl;
 }
