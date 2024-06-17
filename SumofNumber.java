455d0e8c44fe39dfe549e03a9d973dadda701a99
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-06-17 04:54:03 +0000

    sumof tw number

---
+++ SumofNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SumofNumber {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        int x= sc.nextInt();
+        System.out.println("Enter the Second number");
+        int y=sc.nextInt();
+
+        int z=sum(x,y);
+
+        System.out.println(z);
+    }
+
+
+    public static int sum(int x,int y){
+        return x+y;
+    } 
+}
