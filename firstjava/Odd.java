9eb4775a44c8532ef5e3e52b6d7d306a402649f0
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-06-18 05:16:25 +0000

    simple interest program

--- firstjava/Odd.java
+++ firstjava/Odd.java
@@ -2,7 +2,7 @@ package firstjava;
 import java.util.Scanner;
 
 
-public class Main{
+public class Odd{
     public static void main(String [] args){
       Scanner sc= new Scanner(System.in);
       int n=sc.nextInt();
