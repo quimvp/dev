3f935aae6a3cacaa30a883254bbf18261e4985b9
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-06-18 07:02:47 +0000

    greater oftwo number

--- firstjava/TwoNumberLargest.java
+++ firstjava/TwoNumberLargest.java
@@ -1,5 +1,22 @@
 package firstjava;
 
+import java.util.Scanner;
+
 public class TwoNumberLargest {
-    
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the two number to companre");
+        System.out.println("Enter first number");
+        int x= sc.nextInt();
+        System.out.println("Enter second  number");
+        int y= sc.nextInt();
+
+        if(x==y){
+            System.out.println("Equal");
+        }else if(x>y){
+            System.out.println("X is greater");
+        }else{
+            System.out.println("Y is greater");
+        }
+    }
 }
