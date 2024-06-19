a0062807fffbd649a01f10e9f906b5900b044179
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-06-19 17:17:24 +0000

    conditionals

---
+++ conditionals/fibbonacciSeries.java
@@ -0,0 +1,10 @@
+package conditionals;
+
+import java.util.Scanner;
+
+public class fibbonacciSeries {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+    }
+}
