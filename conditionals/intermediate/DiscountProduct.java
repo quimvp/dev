3f88cd2c8bb3b20a1df46844f317a42a9eb5b648
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2024-06-22 05:37:32 +0000

    powerwithException

--- conditionals/intermediate/DiscountProduct.java
+++ conditionals/intermediate/DiscountProduct.java
@@ -3,15 +3,21 @@ package conditionals.intermediate;
 import java.util.Scanner;
 
 public class DiscountProduct {
-    public static void man(String[] args){
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Ente the Product price :: ");
         System.out.println("Enter the Product price: ");
         float price = sc.nextFloat(); 
-        System.out.println("Enter the discount: ");
+        System.out.println("Enter the discount percentage: ");
         float discount = sc.nextFloat(); 
         System.out.println("Product price: " + price);
-        System.out.println("Discount: " + discount);
+        System.out.println("Discount percentage: " + discount);
+
+        float finalprice =price - (price * discount)/100;
+
+        System.out.println("final discounted price is :  "+finalprice);
+
+
       
     }
 }
