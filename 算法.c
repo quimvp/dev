5f71a3d227bf4984d77827ddc0a230b6fd900c83
Xiuzi3 <68059213+Xiuzi3@users.noreply.github.com>
2024-07-09 16:03:13 +0000

    1

--- 算法.c
+++ 算法.c
@@ -1,11 +1,15 @@
-int A[maxn], n;
+/*插入排序算法*/
+#define maxn 1000
 void insertSort(){
+    int A[maxn], n;
     for(int i = 2; i < n; i++){
         int temp = A[i], j = i;
         while(j > 1 && temp < A[j-1]){  //只要temp小于前一个元素A[j-1]
             A[j] = A[j-1];
             j--;
         }
-    }
     A[j] = temp;
+    }
 }
+
+23:57
