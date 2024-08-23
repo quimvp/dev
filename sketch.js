62c1db18827c43fa178673ed782e72455ce6f7ff
ArthurFilipinBonetti <bonettiarthur310@gmail.com>
2024-08-22 22:27:22 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(500, 500);
+  background("black");
+  }
+  
+  function draw() {
+    
+  
+    stroke("white");
+    fill("white");
+  
+    // console.log(mouseIsPressed);
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 30);
+    }
+  }
+
+  
