a5d4bf693885a44b47a0a83480a4eb03dde203fa
Johanna Marcela Orjuela Flórez <61854655+jmorjuelaf@users.noreply.github.com>
2024-10-23 18:20:50 -0500

    first commit

---
+++ matematica_oculta.js
@@ -0,0 +1,19 @@
+// Crea la función "solution" que toma dos parámetros "num1" y "num2"
+function solution(num1,num2) {
+    // Devuelve la resta concatenada con la suma de "num1" y "num2" con return
+    return (num1-num2)+""+(num1+num2);
+  }
+  // Invoca la función "solution" y almacena el resultado en las variables "resultado, resultado1....resultado5"
+  let resultado = solution(5,4);
+  let resultado1 = solution(8,2);
+  let resultado2 = solution(10,8);
+  let resultado3 = solution(12,9);
+  let resultado4 = solution(18,2);
+  let resultado5 = solution(21,5);
+  // Imprime el resultado en la consola
+  console.log(resultado); // Imprime la operación 1
+  console.log(resultado1); // Imprime la operación 2
+  console.log(resultado2); // Imprime la operación 3
+  console.log(resultado3); // Imprime la operación 4
+  console.log(resultado4); // Imprime la operación 5
+  console.log(resultado5); // Imprime la operación 6
